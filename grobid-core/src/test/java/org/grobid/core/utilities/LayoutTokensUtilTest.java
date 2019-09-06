0e252d541f4a7eadec7e38de343ddc1e7be97988
Luca Foppiano <luca@foppiano.org>
2019-09-06 14:17:14 +0900

    improving dehypenisation using coordinates to check breakline

--- grobid-core/src/test/java/org/grobid/core/utilities/LayoutTokensUtilTest.java
+++ grobid-core/src/test/java/org/grobid/core/utilities/LayoutTokensUtilTest.java
@@ -1,6 +1,7 @@
 package org.grobid.core.utilities;
 
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Iterables;
 import org.grobid.core.analyzers.GrobidAnalyzer;
 import org.grobid.core.layout.LayoutToken;
 import org.junit.Test;
@@ -9,6 +10,8 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
+import java.util.stream.IntStream;
+
 import static org.junit.Assert.assertThat;
 
 public class LayoutTokensUtilTest {
@@ -47,4 +50,84 @@ public class LayoutTokensUtilTest {
         assertThat(Iterables.getLast(result).getText(), is("!"));
 
     }
+    /**
+     * We fake the new line in the layout token coordinates
+     */
+    @Test
+    public void testDoesRequireDehyphenization_shouldReturnTrue() throws Exception {
+        String input = "The study of iron-based supercondu- \n" +
+            "ctors superconductivity in the iron-pnictide LaFeAsO 1-x F x has been expanding and has \n";
+
+        List<LayoutToken> layoutTokens = GrobidAnalyzer.getInstance().tokenizeWithLayoutToken(input);
+
+        assertThat(LayoutTokensUtil.doesRequireDehypenisation(layoutTokens, 11), is(true));
+    }
+
+    @Test
+    public void testDoesRequireDehyphenization2_shouldReturnTrue() throws Exception {
+        String input = "The study of iron-based supercondu - \n" +
+            "ctors superconductivity in the iron-pnictide LaFeAsO 1-x F x has been expanding and has \n";
+
+        List<LayoutToken> layoutTokens = GrobidAnalyzer.getInstance().tokenizeWithLayoutToken(input);
+
+        assertThat(LayoutTokensUtil.doesRequireDehypenisation(layoutTokens, 12), is(true));
+    }
+
+    @Test
+    public void testDoesRequireDehyphenization_composedWords_shouldReturnFalse() throws Exception {
+        String input = "The study of iron-based supercondu - \n" +
+            "ctors superconductivity in the iron-pnictide LaFeAsO 1-x F x has been expanding and has \n";
+
+        List<LayoutToken> layoutTokens = GrobidAnalyzer.getInstance().tokenizeWithLayoutToken(input);
+
+        assertThat(LayoutTokensUtil.doesRequireDehypenisation(layoutTokens, 7), is(false));
+        assertThat(LayoutTokensUtil.doesRequireDehypenisation(layoutTokens, 24), is(false));
+    }
+
+    @Test
+    public void testDoesRequireDehyphenization2_composedWords_shouldReturnFalse() throws Exception {
+        String input = "The study of iron- based supercondu - \n" +
+            "ctors superconductivity in the iron-pnictide LaFeAsO 1-x F x has been expanding and has \n";
+
+        List<LayoutToken> layoutTokens = GrobidAnalyzer.getInstance().tokenizeWithLayoutToken(input);
+
+        assertThat(LayoutTokensUtil.doesRequireDehypenisation(layoutTokens, 7), is(false));
+    }
+
+    @Test
+    public void testDoesRequireDehyphenization3_composedWords_shouldReturnFalse() throws Exception {
+        String input = "The study of iron - based supercondu - \n" +
+            "ctors superconductivity in the iron-pnictide LaFeAsO 1-x F x has been expanding and has \n";
+
+        List<LayoutToken> layoutTokens = GrobidAnalyzer.getInstance().tokenizeWithLayoutToken(input);
+
+        assertThat(LayoutTokensUtil.doesRequireDehypenisation(layoutTokens, 8), is(false));
+    }
+
+    @Test
+    public void testDoesRequireDehyphenization_usingCoordinates_shouldReturnTrue() throws Exception {
+        String input = "The study of iron-based supercondu -  " +
+            "ctors superconductivity in the iron-pnictide LaFeAsO 1-x F x has been expanding and has \n";
+
+        List<LayoutToken> layoutTokens = GrobidAnalyzer.getInstance().tokenizeWithLayoutToken(input);
+
+        IntStream.range(0, 15).forEach(i -> layoutTokens.get(i).setY(10));
+        IntStream.range(15, layoutTokens.size()).forEach(i -> layoutTokens.get(i).setY(30));
+
+        assertThat(LayoutTokensUtil.doesRequireDehypenisation(layoutTokens, 12), is(true));
+    }
+
+//    @Test
+//    public void testDoesRequireDehyphenization_withoutNewLine() throws Exception {
+//        String input = "The study of iron-based supercondu -  " +
+//            "ctors superconductivity in the iron-pnictide LaFeAsO 1-x F x has been expanding and has \n";
+//
+//        List<LayoutToken> layoutTokens = GrobidAnalyzer.getInstance().tokenizeWithLayoutToken(input);
+//
+//        IntStream.range(0, 15).forEach(i -> layoutTokens.get(i).setY(10));
+//        IntStream.range(15, layoutTokens.size()).forEach(i -> layoutTokens.get(i).setY(30));
+//
+//        assertThat(LayoutTokensUtil.doesRequireDehypenisation(layoutTokens, 12), is(true));
+//    }
+
 }
